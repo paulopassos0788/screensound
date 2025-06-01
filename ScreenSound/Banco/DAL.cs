49daa512d2cf7b46704b9a4cdd29b3292c8ef48c
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-06-01 16:23:08 -0300

    usando generics
    - Aplicar o conceito de método genérico;
    
    - Refatorar a aplicação para utilizar novos recursos, otimizando código e classes;
    
    - Utilizar o Func para consultar dados através de uma condição variável.

---
+++ ScreenSound/Banco/DAL.cs
@@ -0,0 +1,54 @@
+﻿namespace ScreenSound.Banco;
+
+internal class DAL<T> where T : class
+{
+    protected readonly ScreenSoundContext context;
+
+    public DAL(ScreenSoundContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<T> Listar()
+    {
+        return context.Set<T>().ToList();
+    }
+
+    public void Adicionar(T obj)
+    {
+        context.Set<T>().Add(obj);
+        context.SaveChanges();
+    }
+
+    public void Atualizar(T obj, int id)
+    {
+        var artistaExistente = context.Set<T>().Find(id);
+
+        if (artistaExistente == null)
+        {
+            throw new ArgumentException($"Artista com ID {id} não encontrado.");
+        }
+
+        context.Set<T>().Update(obj);
+        context.SaveChanges();
+    }
+
+    public async Task Excluir(int id)
+    {
+        var artistaExistente = await context.Set<T>().FindAsync(id);
+
+        if (artistaExistente == null)
+        {
+            throw new ArgumentException($"Artista com ID {id} não encontrado.");
+        }
+
+        context.Set<T>().Remove(artistaExistente);
+        await context.SaveChangesAsync();
+    }
+
+    public T? RecuperarPor(Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
+    }
+
+}
--- ScreenSound/Menus/MenuRegistrarMusica.cs
+++ ScreenSound/Menus/MenuRegistrarMusica.cs
@@ -5,7 +5,7 @@ namespace ScreenSound.Menus;
 
 internal class MenuRegistrarMusica : Menu
 {
-    public override void Executar(ArtistaDAL artistaDAL)
+    public override void Executar(DAL<Artista> artistaDAL)
     {
         base.Executar(artistaDAL);
         ExibirTituloDaOpcao("Registro de músicas");
@@ -13,7 +13,7 @@ internal class MenuRegistrarMusica : Menu
         Console.Write("Digite o artista cuja música deseja registrar: ");
         string nomeDoArtista = Console.ReadLine()!;
 
-        var artistaRecuperado = artistaDAL.PesquisarArtistaPorNome(nomeDoArtista);
+        var artistaRecuperado = artistaDAL.RecuperarPor(a => a.Nome.Equals(nomeDoArtista));
 
         if (artistaRecuperado is not null)
         {
